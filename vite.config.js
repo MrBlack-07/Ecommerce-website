36c1a05b456476b2eb343c334ac82b9db5a76254
Appun007 <appunappu76@gmail.com>
2025-04-29 09:13:39 +0530

    Deployment fix

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'bootstrap-vendor': ['bootstrap', 'bootstrap-icons'],
+        },
+      },
+    },
   },
   assetsInclude: ['**/*.PNG', '**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'],
 })
